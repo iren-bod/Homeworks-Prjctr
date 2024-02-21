30ec60953b30c1d201863014963f8e5abaa256a4
Iryna <bodak.iryna@gmail.com>
2024-02-21 11:59:27 +0100

    created a function that finds the sum of all arguments and a function that multiplies the arguments using a closure

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 "use strict";
+
+//Task 1
+console.log(addThemAll(2, 4));
+console.log(addThemAll(1, 2, 3, 4));
+console.log(addThemAll(5, 5, 10));
+
+function addThemAll(...args) {
+    let sum = 0;
+    for (let num of args) {
+        sum += num;
+    }
+    return sum;
+}
+
+
+//Task 2
+console.log(multiply(5)(5));
+console.log(multiply(2)(-2));
+console.log(multiply(4)(3));
+
+function multiply(a) {
+    return function (b) {
+        return a * b;
+    }
+}
