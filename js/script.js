4de527d8a65913d60ffb0d8c6cd454981b1d1a40
Iryna <bodak.iryna@gmail.com>
2024-03-12 21:05:02 +0100

    I completed the homework from the DOM workshop

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+//для елементу з текстом 'Навігація по DOM дереву'
+let headerTwo = document.getElementById("headerTwo");
+console.log(headerTwo);
+
+//для першого елементу <section>
+let section = document.querySelector(".firstSection");
+console.log(section);
+
+//для елементу списку з текстом 'Пункт 5'
+let listItemFive = document.querySelector('li:nth-of-type(5)');
+console.log(listItemFive);
+
+//для елементу з класом 'hatredLevelBlock'
+let hatredLevelBlock = document.querySelector('.hatredLevelBlock');
+console.log(hatredLevelBlock);
